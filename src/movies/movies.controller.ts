92b3c6436ff64eabc2f043a6abf6900658f0922e
park0503 <hooo0503@gmail.com>
2021-12-09 17:09:11 +0900

    divide by module

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Req, Res } from '@nestjs/common';
 import { createMovieDTO } from './dto/create-movie.dto';
 import { updateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
