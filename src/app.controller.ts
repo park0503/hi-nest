4b025f7f1f75dcab3997bd75aea0a86ef5a3ea9f
park0503 <hooo0503@gmail.com>
2021-12-26 02:26:20 +0900

    auto2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   home() {
-    return { connection: "success - auto" };
+    return { connection: "success - auto2" };
   }
 }
