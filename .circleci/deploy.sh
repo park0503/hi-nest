446ce7bd9341b2550dc05d927f4316cfd0d71bfc
park0503 <hooo0503@gmail.com>
2021-12-25 17:44:12 +0900

    circle

---
+++ .circleci/deploy.sh
@@ -0,0 +1,37 @@
+name="test"
+
+aws --version
+aws configure set default.aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set default.aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set default.region "$AWS_DEFAULT_REGION"
+aws configure set default.output json
+
+container_definition="[
+  {
+    \"name\": \"$name\",
+    \"image\": \"$REPOSITORY_URL\",
+    \"repositoryCredentials\": {
+      \"credentialsParameter\": \"$REPOSITORY_CREDENTIAL\"
+    },
+    \"portMappings\": [
+      {
+        \"containerPort\": 8080,
+        \"hostPort\": 8080,
+        \"protocol\": \"tcp\"
+      }
+    ],
+    \"environment\":[]
+  }
+]"
+
+task_definition=$(aws ecs register-task-definition \
+--container-definitions "$container_definition" \
+--family test \
+--execution-role-arn "arn:aws:iam::$AWS_ACCOUNT_ID:role/ecsTaskExecutionRole" \
+--task-role-arn "arn:aws:iam::$AWS_ACCOUNT_ID:role/ecsTaskExecutionRole" \
+--network-mode "awsvpc" \
+--requires-compatibilities "FARGATE" \
+--cpu "$AWS_ECS_CONTAINER_CPU" \
+--memory "$AWS_ECS_CONTAINER_MEMORY")
+
+echo 
